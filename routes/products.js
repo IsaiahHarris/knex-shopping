0ed464e3db5bb26a650d594c3ce8bb78ce70ef86
Isaiah Harris <harrisfambam@gmail.com>
2018-08-02 20:12:01 -1000

    finish second product get

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,38 @@ const db = require('../db/knex');
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-  res.send('you got products');
+  return db.raw('SELECT * FROM products')
+  .then(result=>{
+    if(!result || !result.rowCount){
+      res.status(404).json({"message": "there are no products"})
+    }
+  })
+  .then(result=>{
+    return res.json(result.rows);
+  })
+  .catch(err=>{
+    console.log('there has been an error')
+    res.send('there has been an error');
+  })
 })
+
+router.get('/:product_id', (req, res)=>{
+  const id = req.params.product_id;
+  db.raw('SELECT id FROM products WHERE id = ?', [id])
+  .then(result=>{
+    if(!result || !result.rowCount){
+      return res.json({ "message": "Product not found" })
+    }
+    return result;
+  })
+  .then(result=>{
+    return res.json(result.rows);
+  })
+  .catch(err=>{
+    console.log(err);
+    res.send('there has been an error');
+  })
+})
+
 
 module.exports = router;
