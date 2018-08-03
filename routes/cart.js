5cfb8fa4ab7972a0735a5ea8102fb39e472277aa
Isaiah Harris <harrisfambam@gmail.com>
2018-08-02 16:10:04 -1000

    set endpoints

---
+++ routes/cart.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const db = require('../db/knex');
+const router = express.Router();
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cart = require('./routes/cart');
+const users = require('./routes/users');
+const products = require('./routes/products');
 const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}));
@@ -7,6 +10,9 @@ app.get('/', (req,res)=>{
   res.send('smoke test');
 })
 
+app.use('/cart', cart)
+app.use('/users', users)
+app.use('/products', products)
 app.listen(PORT, ()=>{
   console.log('listening to port 3000');
 })
