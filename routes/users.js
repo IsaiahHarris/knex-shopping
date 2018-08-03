28a50f04b35d69dcfc5455b3a390cdfd869aed7f
Isaiah Harris <harrisfambam@gmail.com>
2018-08-02 16:45:09 -1000

    user get finished

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,22 @@ const express = require('express');
 const db = require('../db/knex');
 const router = express.Router();
 
-router.get('/', (req,res)=>{
+router.get('/', (req, res) => {
   res.send('you got users');
+})
+
+router.get('/:user_id', (req, res) => {
+  const id = req.params.id;
+  db.raw('SELECT email FROM users WHERE id = ?', [id])
+    .then(user => {
+      if (!user || !user.rowCount) {
+        res.status(400).send("{ message: 'User not found }");
+      }
+      res.json(user.rows[0])
+    })
+    .catch(err=>{
+      res.send('there was an error');
+    })
 })
 
 module.exports = router;
