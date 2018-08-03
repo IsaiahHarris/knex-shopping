ca4163c7e36fcfd1544818239796a62a72a6206f
Isaiah Harris <harrisfambam@gmail.com>
2018-08-02 16:21:26 -1000

    can start making tables

--- routes/products.js
+++ routes/products.js
@@ -2,4 +2,8 @@ const express = require('express');
 const db = require('../db/knex');
 const router = express.Router();
 
+router.get('/', (req,res)=>{
+  res.send('you got products');
+})
+
 module.exports = router;
